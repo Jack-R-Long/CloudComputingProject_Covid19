f49d5f5b844456d6d54f8f0df1cb2699c8c736eb
jack.long7 <jack.richard.long@gmail.com>
2020-11-09 18:01:39 -0600

    Added MDBoostrap HTML template

--- covid19/app/home/views.py
+++ covid19/app/home/views.py
@@ -6,3 +6,10 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, "home/index.html")
+
+# ------------------------------------------------------------------
+# Bootstrap Template
+# ------------------------------------------------------------------
+
+def boot(request):
+	return render(request, "home/formMD.html")
